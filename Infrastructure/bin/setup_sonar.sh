cfa110cb2f9ab783618a5012cdb59f0aba762dc8
AnanthCapiot <41581468+AnanthCapiot@users.noreply.github.com>
2018-09-04 12:07:36 +0530

    Update setup_sonar.sh

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Setup Sonarqube Project
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     echo "Usage:"
-    echo "  $0 GUID"
+    echo "  $0 GUID USER"
     exit 1
 fi
 
 GUID=$1
+USER=$2
+
 echo "Setting up Sonarqube in project $GUID-sonarqube"
 
 # Code to set up the SonarQube project.
@@ -14,3 +16,4 @@ echo "Setting up Sonarqube in project $GUID-sonarqube"
 # oc new-app -f ../templates/sonarqube.yaml --param .....
 
 # To be Implemented by Student
+oc new-project xyz-sonarqube --display-name "Shared Sonarqube"
