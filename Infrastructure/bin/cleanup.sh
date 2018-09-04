bfacd1e5ee230108187b06075a34bf2ec871ab41
AnanthCapiot <41581468+AnanthCapiot@users.noreply.github.com>
2018-09-04 11:10:15 +0530

    Update cleanup.sh

--- Infrastructure/bin/cleanup.sh
+++ Infrastructure/bin/cleanup.sh
@@ -8,7 +8,7 @@ fi
 
 GUID=$1
 echo "Removing all Homework Projects for GUID=$GUID"
-oc login -u ananth-capiot.com -p securityPolicy@1234 https://master.na39.openshift.opentlc.com
+oc login https://master.na39.openshift.opentlc.com --token=n7QTfvDc7ExFrFuPlqM8-rbWtkwnjiY11e0CQ7VIPfQ
 oc delete project $GUID-nexus
 oc delete project $GUID-sonarqube
 oc delete project $GUID-jenkins
