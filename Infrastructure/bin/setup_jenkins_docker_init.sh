7e9007208663a131f776d051da8019c206758f53
AnanthCapiot <41581468+AnanthCapiot@users.noreply.github.com>
2018-09-05 12:19:22 +0530

    Create setup_jenkins_docker_init.sh

---
+++ Infrastructure/bin/setup_jenkins_docker_init.sh
@@ -0,0 +1,11 @@
+# Backup existing registries.conf to /etc/containers/registries.conf.yyyyMMddHHMM
+echo "Backup existing registries.conf to /etc/containers/registries.conf.yyyyMMddHHMM"
+cp /etc/containers/registries.conf /etc/containers/registries.conf.$(date +%Y%m%d%H%M)
+cd /home/{USER}
+command cp -fr registries.conf /etc/containers/
+
+echo "Enabling Docker and Starting Docker service"
+systemctl enable docker
+systemctl start docker
+
+echo "Docker services started..."
