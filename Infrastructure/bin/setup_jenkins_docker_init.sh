4dcdf680a77c2f6b90a7b6e1d386dfe9e3693dcd
AnanthCapiot <41581468+AnanthCapiot@users.noreply.github.com>
2018-09-06 19:51:32 +0530

    Update setup_jenkins_docker_init.sh

--- Infrastructure/bin/setup_jenkins_docker_init.sh
+++ Infrastructure/bin/setup_jenkins_docker_init.sh
@@ -26,8 +26,3 @@ echo "Docker file created"...
 
 docker build . -t docker-registry-default.apps.${GUID}.openshift.opentlc.com/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.9 && \
 echo "Docker build completed..."
-
-echo ${USER}:$(${USER} -t)
-
-echo "skopeo copy --dest-tls-verify=false --dest-creds=$(oc whoami):$(oc whoami -t) docker-daemon:docker-registry-default.apps.${GUID}.openshift.opentlc.com/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.9 docker://docker-registry-default.apps.${GUID}.openshift.opentlc.com/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.9"
-echo "skopeo copy to Docker registry successful..."
