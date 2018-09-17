dcb322a7c406cd4e892e0675950539c3accd7269
AnanthCapiot <41581468+AnanthCapiot@users.noreply.github.com>
2018-09-17 15:00:27 +0530

    Update setup_dev.sh

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -18,28 +18,27 @@ oc project ${GUID}-parks-dev
 echo "Building Mongo DB Project"
 
 cd $HOME/eb90AdvDevHomework/Infrastructure/templates
-oc create -f dev-mongodb-configmaps.yml
-oc create -f dev-mongodb-template.yml
-
-oc create -f dev-mlb-parks-app-config-map.yml
-oc create -f dev-national-parks-app-config-map.yml
-oc create -f dev-parks-map-app-config-map.yml
+oc create -f dev-mongodb-configmaps.yml && \
+oc create -f dev-mlb-parks-app-config-map.yml && \
+oc create -f dev-national-parks-app-config-map.yml && \
+oc create -f dev-parks-map-app-config-map.yml && \
+oc create -f dev-mongodb-template.yml && \
 
 # Building MLBParks application
 git clone https://github.com/AnanthCapiot/${GUID}AdvDevHomework.git
 cd $HOME/${GUID}AdvDevHomework/MLBParks/
-mvn -s ../nexus_settings.xml clean package -DskipTests=true
+mvn -s ../nexus_settings.xml clean package -DskipTests=true && \
 
 # Do we need to start and test the app? (like in our labs)
 
 echo "Create a binary build called mlbparks-binary"
-oc new-build --binary=true --name=mlbparks-binary --image-stream=jboss-eap70-openshift:1.7
+oc new-build --binary=true --name=mlbparks-binary --image-stream=jboss-eap70-openshift:1.7 && \
 
 echo "Starting build and streaming compiled war file to Build"
-oc start-build mlbparks-binary --from-file=$HOME/${GUID}AdvDevHomework/MLBParks/target/mlbparks.war --follow
+oc start-build mlbparks-binary --from-file=$HOME/${GUID}AdvDevHomework/MLBParks/target/mlbparks.war --follow && \
 
-oc new-app mlbparks-binary
-oc expose svc/mlbparks-binary --port=8080
+oc new-app mlbparks-binary && \
+oc expose svc/mlbparks-binary --port=8080 && \
 
 curl http://$(oc get route mlbparks-binary --template='{{ .spec.host }}')/ws/healthz/
 curl http://$(oc get route mlbparks-binary --template='{{ .spec.host }}')/ws/info/
