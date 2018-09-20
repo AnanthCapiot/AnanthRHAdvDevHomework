7a39fcdaecba802ec6c1a77f4295567269d26450
AnanthCapiot <41581468+AnanthCapiot@users.noreply.github.com>
2018-09-20 22:48:02 +0530

    Update setup_prod.sh

--- Infrastructure/bin/setup_prod.sh
+++ Infrastructure/bin/setup_prod.sh
@@ -26,10 +26,19 @@ echo "Creating Stateful Set for MongoDB" && \
 oc create -f prod-mongodb-statefulset.yml && \
 
 oc get pvc && \
+echo "StatefulSet MongoDB created Successfully"
 
 # Code to set up the parks production project. It will need a StatefulSet MongoDB, and two applications each (Blue/Green) for NationalParks, MLBParks and Parksmap.
 # The Green services/routes need to be active initially to guarantee a successful grading pipeline run.
 
 # To be Implemented by Student
 
+echo ">>>> Creating Blue Application environment for MLBParks Application"
+# Create Blue Application
+oc new-app mlbparks/mlbparks:0.0 --name=mlbparks-blue --allow-missing-imagestream-tags=true -n ${GUID}-parks-prod
+oc set triggers dc/mlbparks-blue --remove-all -n ${GUID}-parks-prod
+oc expose dc mlbparks-blue --port 8080 -n ${GUID}-parks-prod
+
+# Expose Blue service as route to make blue application active
+oc expose svc/mlbparks-blue --name mlbparks -n ${GUID}-mlbparks-prod
 
