04a3a0b927f33f44c46c97ec8d8189e632dfacb7
AnanthCapiot <41581468+AnanthCapiot@users.noreply.github.com>
2018-09-20 19:27:43 +0530

    Update setup_dev.sh

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -37,7 +37,7 @@ oc env dc/mongodb --from=configmap/dev-mongodb-config-map && \
 
 oc create -f dev-mongodb-pvc-template.yml && \
 
-oc set probe dc/mongodb --readiness --failure-threshold 3 --initial-delay-seconds 60 -- "mongo 127.0.0.1:27017/parks -u mongodb -p mongodb --eval='quit()'" && \
+oc set probe dc/mongodb --readiness --failure-threshold 3 --initial-delay-seconds 60 -- "oc set probe dc/mongodb --readiness --failure-threshold 3 --initial-delay-seconds 60 -- "/bin/sh '-i' '-c' > - mongo 127.0.0.1:27017/parks -u mongodb -p mongodb --eval='quit()'" && \
 
 oc rollout resume dc/mongodb && \
 
