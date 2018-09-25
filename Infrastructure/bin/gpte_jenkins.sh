140b64d79ce4108fa43e82b8a78759475dd12b56
AnanthCapiot <41581468+AnanthCapiot@users.noreply.github.com>
2018-09-25 18:53:11 +0530

    Update gpte_jenkins.sh

--- Infrastructure/bin/gpte_jenkins.sh
+++ Infrastructure/bin/gpte_jenkins.sh
@@ -11,16 +11,16 @@ USER=$2
 echo "Creating all Homework Projects for GUID=${GUID} and USER=${USER}"
 oc login https://master.na39.openshift.opentlc.com -u ${USER} -p securityPolicy@1234
 
-oc new-project gpte-jenkins2 --display-name="My Homework Grading Jenkins"
+oc new-project gpte-jenkins --display-name="My Homework Grading Jenkins"
 
-oc project gpte-jenkins2
+oc project gpte-jenkins
 
-oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi -n gpte-jenkins2
-oc set resources dc/jenkins --limits=cpu=1,memory=3Gi --requests=memory=2Gi,cpu=1 -n gpte-jenkins2
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi -n gpte-jenkins
+oc set resources dc/jenkins --limits=cpu=1,memory=3Gi --requests=memory=2Gi,cpu=1 -n gpte-jenkins
 
 oc create clusterrole namespace-patcher --verb=patch --resource=namespaces
-oc adm policy add-cluster-role-to-user namespace-patcher system:serviceaccount:gpte-jenkins2:jenkins
-oc adm policy add-cluster-role-to-user self-provisioner system:serviceaccount:gpte-jenkins2:jenkins
+oc adm policy add-cluster-role-to-user namespace-patcher system:serviceaccount:gpte-jenkins:jenkins
+oc adm policy add-cluster-role-to-user self-provisioner system:serviceaccount:gpte-jenkins:jenkins
 
-oc policy add-role-to-user edit system:serviceaccount:gpte-jenkins2:default -n gpte-jenkins2
-oc run restartjenkins --schedule="0 23 * * *" --restart=OnFailure -n gpte-jenkins2 --image=registry.access.redhat.com/openshift3/jenkins-2-rhel7:v3.9 -- /bin/sh -c "oc scale dc jenkins --replicas=0 && sleep 20 && oc scale dc jenkins --replicas=1"
+oc policy add-role-to-user edit system:serviceaccount:gpte-jenkins:default -n gpte-jenkins
+oc run restartjenkins --schedule="0 23 * * *" --restart=OnFailure -n gpte-jenkins --image=registry.access.redhat.com/openshift3/jenkins-2-rhel7:v3.9 -- /bin/sh -c "oc scale dc jenkins --replicas=0 && sleep 20 && oc scale dc jenkins --replicas=1"
