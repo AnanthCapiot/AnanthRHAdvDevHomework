dd535d6f6a8aa78127a1bd18a764315e3c8cb5ca
AnanthCapiot <41581468+AnanthCapiot@users.noreply.github.com>
2018-09-25 12:48:54 +0530

    Update reset_prod.sh

--- Infrastructure/bin/reset_prod.sh
+++ Infrastructure/bin/reset_prod.sh
@@ -18,3 +18,7 @@ echo "Resetting Parks Production Environment in project ${GUID}-parks-prod to Gr
 # rollout followed by a Green rollout.
 
 # To be Implemented by Student
+
+sh 'oc patch route mlbparks -n ${GUID}-parks-prod -p \'{"spec":{"to":{"name":"mlbparks-green"}}}\''
+sh 'oc patch route nationalparks -n ${GUID}-parks-prod -p \'{"spec":{"to":{"name":"nationalparks-green"}}}\''
+sh 'oc patch route parksmap -n ${GUID}-parks-prod -p \'{"spec":{"to":{"name":"parksmap-green"}}}\''
