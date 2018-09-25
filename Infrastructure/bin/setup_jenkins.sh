63999a6a30d4cc05f064f229bfcc69a1afb241f1
AnanthCapiot <41581468+AnanthCapiot@users.noreply.github.com>
2018-09-25 13:30:41 +0530

    Update setup_jenkins.sh

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -30,7 +30,9 @@ oc project ${GUID}-jenkins
 # * CLUSTER: the base url of the cluster used (e.g. na39.openshift.opentlc.com)
 
 # To be Implemented by Student
-oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi -n ${GUID}-jenkins
+
+oc set resources dc/jenkins --limits=cpu=2 --requests=memory=2Gi,cpu=1 -n ${GUID}-jenkins
 
 oc set probe dc jenkins --readiness --initial-delay-seconds=500
 
