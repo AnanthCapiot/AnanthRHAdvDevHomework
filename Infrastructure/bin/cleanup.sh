27c3573c6a97faf4c2f343f064d0f980ca5f4614
AnanthCapiot <41581468+AnanthCapiot@users.noreply.github.com>
2018-09-25 15:42:43 +0530

    Update cleanup.sh

--- Infrastructure/bin/cleanup.sh
+++ Infrastructure/bin/cleanup.sh
@@ -2,12 +2,11 @@
 # Delete all Homework Projects
 if [ "$#" -ne 2 ]; then
     echo "Usage:"
-    echo "  $0 GUID USER"
+    echo "  $0 GUID"
     exit 1
 fi
 
 GUID=$1
-USER=$2
 
 echo "Removing all Homework Projects for GUID=$GUID"
 oc login https://master.na39.openshift.opentlc.com -u ${USER} -p securityPolicy@1234
