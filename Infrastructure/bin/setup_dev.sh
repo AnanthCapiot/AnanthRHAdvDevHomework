630d62e324d935fa68a082b211885ff3afb3b70d
AnanthCapiot <41581468+AnanthCapiot@users.noreply.github.com>
2018-09-26 19:31:43 +0530

    Update setup_dev.sh

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -7,94 +7,61 @@ if [ "$#" -ne 1 ]; then
 fi
 
 GUID=$1
-
 echo "Setting up Parks Development Environment in project ${GUID}-parks-dev"
 
-git pull https://github.com/AnanthCapiot/AnanthRHAdvDevHomework.git
-#git reset --hard HEAD && git pull origin master
-sudo chmod +x *.sh
-
-# Code to set up the parks development project.
 oc project ${GUID}-parks-dev
+# Code to set up the parks development project.
 
-echo "Setting Policy for Jenkins user to ${GUID}-parks-dev project"
-oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-parks-dev
+oc policy add-role-to-user edit system:serviceaccount:70fa-jenkins:jenkins -n 70fa-parks-dev
 
-echo "Trying Git clone for project sources"
-#git clone https://github.com/AnanthCapiot/${GUID}AdvDevHomework.git
+oc new-app --name=mongodb -e MONGODB_USER=mongodb -e MONGODB_PASSWORD=mongodb -e MONGODB_DATABASE=parks -e MONGODB_ADMIN_PASSWORD=mongodb_admin_password     registry.access.redhat.com/rhscl/mongodb-26-rhel7
+oc rollout pause dc/mongodb 
+echo "apiVersion: "v1"
+kind: "PersistentVolumeClaim"
+metadata:
+  name: "mongo-pvc"
+spec:
+  accessModes:
+    - "ReadWriteOnce"
+  resources:
+    requests:
+      storage: "2Gi"" | oc create -f -
 
-# To be Implemented by Student
-echo "Building Mongo DB Project"
+oc set volume dc/mongodb --add --type=persistentVolumeClaim --name=mongo-pv --claim-name=mongo-pvc --mount-path=/data --containers=*
+oc rollout resume dc/mongodb
 
-cd $HOME/AnanthRHAdvDevHomework/Infrastructure/templates
-oc create -f dev-mongodb-configmaps.yml && \
 
-oc new-app --name=mongodb -e MONGODB_USER=mongodb -e MONGODB_PASSWORD=mongodb -e MONGODB_DATABASE=parks -e MONGODB_ADMIN_PASSWORD=mongodb registry.access.redhat.com/rhscl/mongodb-26-rhel7 && \
+oc new-build --binary=true --strategy=source --name=mlbparks jboss-eap70-openshift:1.7 
+oc new-build --binary=true --strategy=source --name=nationalparks redhat-openjdk18-openshift:1.2
+oc new-build --binary=true --strategy=source --name=parksmap redhat-openjdk18-openshift:1.2
 
-oc rollout pause dc/mongodb && \
+oc new-app 70fa-parks-dev/mlbparks:0.0-0 -e APPNAME="MLB Parks (Dev)" --name=mlbparks --allow-missing-imagestream-tags=true 
+oc new-app 70fa-parks-dev/nationalparks:0.0-0 -e APPNAME="National Parks (Dev)" --name=nationalparks --allow-missing-imagestream-tags=true 
+oc new-app 70fa-parks-dev/parksmap:0.0-0 -e APPNAME="ParksMap (Dev)" --name=parksmap --allow-missing-imagestream-tags=true 
 
-oc env dc/mongodb --from=configmap/dev-mongodb-config-map && \
+oc set triggers dc/mlbparks --remove-all
+oc set triggers dc/nationalparks --remove-all
+oc set triggers dc/parksmap --remove-all
 
-oc create -f dev-mongodb-pvc-template.yml && \
+oc expose dc mlbparks --port 8080
+oc expose dc nationalparks --port 8080
+oc expose dc parksmap --port 8080
 
-#oc set probe dc/mongodb --readiness --failure-threshold 3 --initial-delay-seconds 60 -- "/bin/sh '-i' '-c' > - mongo 127.0.0.1:27017/parks -u mongodb -p mongodb --eval='quit()'" && \
+oc expose svc mlbparks -l type=parksmap-backend
+oc expose svc nationalparks  -l type=parksmap-backend
+oc expose svc parksmap  -l type=parksmap-backend
 
-oc rollout resume dc/mongodb && \
+echo "DB_HOST=mongodb
+DB_PORT=27017
+DB_USERNAME=mongodb
+DB_PASSWORD=mongodb
+DB_NAME=parks" > application-db.properties
 
-echo "Setting up MLBParks Application"
-# Building MLBParks application
-cd $HOME/AnanthRHAdvDevHomework/MLBParks/
+oc create configmap mlbparks-config --from-literal="application-db.properties=Placeholder"
+oc create configmap nationalparks-config --from-literal="application-db.properties=Placeholder"
 
-# Set up Dev Application
-oc new-build --binary=true --name="mlbparks" jboss-eap70-openshift:1.7 -n ${GUID}-parks-dev && \
-
-oc new-app ${GUID}-parks-dev/mlbparks:0.0-0 --name=mlbparks --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev -e APPNAME="MLB Parks (Dev)" && \
-
-oc set triggers dc/mlbparks --remove-all -n ${GUID}-parks-dev && \
-
-oc create configmap dev-application-mongodb-config-map --from-literal="dev-mongodb-connection.properties=Placeholder" -n ${GUID}-parks-dev \n
-
-oc env dc/mlbparks --from=configmap/dev-application-mongodb-config-map -n ${GUID}-parks-dev && \
-
-oc expose dc mlbparks --port 8080 -n ${GUID}-parks-dev && \
-
-oc expose svc mlbparks -n ${GUID}-parks-dev -l type=parksmap-backend && \
-
-echo ">>>>>>>> Completed exposing MLB Parks application/service successfully <<<<<<<<<"
-
-echo "Begin building of National Parks application..."
-cd $HOME/AnanthRHAdvDevHomework/Nationalparks/
-
-oc new-build --binary=true --name=nationalparks --image-stream=redhat-openjdk18-openshift:1.2 && \
-
-oc new-app ${GUID}-parks-dev/nationalparks:0.0-0 --name=nationalparks --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev -e APPNAME="National Parks (Dev)" && \
-
-oc set triggers dc/nationalparks --remove-all -n ${GUID}-parks-dev && \
-
-oc env dc/nationalparks --from=configmap/dev-application-mongodb-config-map -n ${GUID}-parks-dev && \
-
-oc expose dc nationalparks --port 8080 -n ${GUID}-parks-dev && \
-
-oc expose svc nationalparks -n ${GUID}-parks-dev -l type=parksmap-backend && \
-
-echo ">>>>>>>> Completed exposing NationalParks application/service successfully <<<<<<<<<"
-
-echo "Begin building of Parks Map application..."
-cd $HOME/AnanthRHAdvDevHomework/ParksMap
-
-oc new-build --binary=true --name=parksmap --image-stream=redhat-openjdk18-openshift:1.2 && \
-
-oc new-app ${GUID}-parks-dev/parksmap:0.0-0 --name=parksmap --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev -e APPNAME="Parks Map (Dev)" && \
-
-oc set triggers dc/parksmap --remove-all -n ${GUID}-parks-dev && \
-
-oc env dc/parksmap --from=configmap/dev-application-mongodb-config-map -n ${GUID}-parks-dev && \
-
-oc expose dc parksmap --port 8080 -n ${GUID}-parks-dev && \
-
-oc expose svc parksmap -n ${GUID}-parks-dev -l type=parksmap-backend && \
-
-echo ">>>>>>>> Completed exposing Parks Map application/service successfully <<<<<<<<<"
+# oc env dc/mlbparks --from=configmap/mlbparks-config
+# oc env dc/nationalparks --from=configmap/nationalparks-config
 
 oc patch dc/mlbparks --patch "spec: { strategy: {type: Rolling, rollingParams: {post: {failurePolicy: Ignore, execNewPod: {containerName: mlbparks, command: ['curl -XGET http://localhost:8080/ws/data/load/']}}}}}"
 oc patch dc/nationalparks --patch "spec: { strategy: {type: Rolling, rollingParams: {post: {failurePolicy: Ignore, execNewPod: {containerName: nationalparks, command: ['curl -XGET http://localhost:8080/ws/data/load/']}}}}}"
@@ -104,6 +71,9 @@ oc set probe dc/mlbparks --readiness --failure-threshold 3 --initial-delay-secon
 
 oc set probe dc/nationalparks --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok
 oc set probe dc/nationalparks --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/
+
+# oc set volume dc/mlbparks --add --name=mlbparks-config-vol --mount-path=/configuration/application-db.properties --sub-path=application-db.properties --configmap-name=mlbparks-config
+# oc set volume dc/nationalparks --add --name=nationalparks-config-vol --mount-path=/configuration/application-db.properties --sub-path=application-db.properties --configmap-name=nationalparks-config
 
 echo "apiVersion: v1
 kind: Service
@@ -117,3 +87,5 @@ spec:
     ports:
     - protocol: TCP
       port: 8080" | oc create -f -
+# oc expose svc tasks
+# To be Implemented by Student
