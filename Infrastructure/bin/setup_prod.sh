ad02fd59f30bc5be852d90f6f34c1e641e754dc7
AnanthCapiot <41581468+AnanthCapiot@users.noreply.github.com>
2018-09-27 18:38:17 +0530

    Update setup_prod.sh

--- Infrastructure/bin/setup_prod.sh
+++ Infrastructure/bin/setup_prod.sh
@@ -19,106 +19,26 @@ oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n
 # To be Implemented by Student
 
 # Replicated MongoDB setup
-echo 'kind: Service
-apiVersion: v1
-metadata:
-  name: "mongodb-internal"
-  labels:
-    name: "mongodb"
-  annotations:
-    service.alpha.kubernetes.io/tolerate-unready-endpoints: "true"
-spec:
-  clusterIP: None
-  ports:
-    - name: mongodb
-      port: 27017
-  selector:
-    name: "mongodb"' | oc create -f -
+echo "Creating Headless Service"
+oc create -f prod-mongodb-headless-service.yml && \
 
+echo "Creating Regular MongoDB Service" && \
+oc create -f prod-mongodb-regular-service.yml && \
 
+echo "Creating Stateful Set for MongoDB" && \
+oc create -f prod-mongodb-statefulset.yml && \
 
-echo 'kind: Service
-apiVersion: v1
-metadata:
-  name: "mongodb"
-  labels:
-    name: "mongodb"
-spec:
-  ports:
-    - name: mongodb
-      port: 27017
-  selector:
-    name: "mongodb"' | oc create -f -
-
-
-
-echo 'kind: StatefulSet
-apiVersion: apps/v1
-metadata:
-  name: "mongodb"
-spec:
-  serviceName: "mongodb-internal"
-  replicas: 3
-  selector:
-    matchLabels:
-      name: mongodb
-  template:
-    metadata:
-      labels:
-        name: "mongodb"
-    spec:
-      containers:
-        - name: mongo-container
-          image: "registry.access.redhat.com/rhscl/mongodb-34-rhel7:latest"
-          ports:
-            - containerPort: 27017
-          args:
-            - "run-mongod-replication"
-          volumeMounts:
-            - name: mongo-data
-              mountPath: "/var/lib/mongodb/data"
-          env:
-            - name: MONGODB_DATABASE
-              value: "parks"
-            - name: MONGODB_USER
-              value: "mongodb"
-            - name: MONGODB_PASSWORD
-              value: "mongodb"
-            - name: MONGODB_ADMIN_PASSWORD
-              value: "mongodb_admin_password"
-            - name: MONGODB_REPLICA_NAME
-              value: "rs0"
-            - name: MONGODB_KEYFILE_VALUE
-              value: "12345678901234567890"
-            - name: MONGODB_SERVICE_NAME
-              value: "mongodb-internal"
-          readinessProbe:
-            exec:
-              command:
-                - stat
-                - /tmp/initialized
-  volumeClaimTemplates:
-    - metadata:
-        name: mongo-data
-        labels:
-          name: "mongodb"
-      spec:
-        accessModes: [ ReadWriteOnce ]
-        resources:
-          requests:
-            storage: "4Gi"' | oc create -f -
-
+oc get pvc && \
+echo "StatefulSet MongoDB created Successfully"
 
 # Blue Application
 oc new-app ${GUID}-parks-dev/mlbparks:0.0 --name=mlbparks-blue -e APPNAME="MLB Parks (Blue)" --allow-missing-imagestream-tags=true
 oc new-app ${GUID}-parks-dev/nationalparks:0.0 --name=nationalparks-blue -e APPNAME="National Parks (Blue)" --allow-missing-imagestream-tags=true
 oc new-app ${GUID}-parks-dev/parksmap:0.0 --name=parksmap-blue -e APPNAME="ParksMap (Blue)" --allow-missing-imagestream-tags=true
 
-
 oc set triggers dc/mlbparks-blue --remove-all
 oc set triggers dc/nationalparks-blue --remove-all
 oc set triggers dc/parksmap-blue --remove-all
-
 
 oc expose dc/mlbparks-blue --port 8080
 oc expose dc/nationalparks-blue --port 8080
