ce85071e5d2e48b8df7ae771ed03a57c42fedd5b
AnanthCapiot <41581468+AnanthCapiot@users.noreply.github.com>
2018-09-27 15:31:52 +0530

    Update setup_projects.sh

--- Infrastructure/bin/setup_projects.sh
+++ Infrastructure/bin/setup_projects.sh
@@ -1,161 +1,38 @@
 #!/bin/bash
-# Setup Production Project (initial active services: Green)
-if [ "$#" -ne 1 ]; then
+# Create all Homework Projects
+if [ "$#" -ne 2 ]; then
     echo "Usage:"
-    echo "  $0 GUID"
+    echo "  $0 GUID USER"
     exit 1
 fi
 
 GUID=$1
-echo "Setting up Parks Production Environment in project ${GUID}-parks-prod"
+USER=$2
+echo "Creating all Homework Projects for GUID=${GUID} and USER=${USER}"
+oc login https://master.na39.openshift.opentlc.com -u ${USER} -p securityPolicy@1234
 
-oc project ${GUID}-parks-prod
-# Code to set up the parks production project. It will need a StatefulSet MongoDB, and two applications each (Blue/Green) for NationalParks, MLBParks and Parksmap.
-# The Green services/routes need to be active initially to guarantee a successful grading pipeline run.
+oc new-project ${GUID}-nexus        --display-name="${GUID} AdvDev Homework Nexus"
+oc new-project ${GUID}-sonarqube    --display-name="${GUID} AdvDev Homework Sonarqube"
+oc new-project ${GUID}-jenkins    --display-name="${GUID} AdvDev Homework Jenkins"
 
-oc policy add-role-to-group system:image-puller system:serviceaccounts:70fa-parks-prod -n 70fa-parks-dev
-oc policy add-role-to-user edit system:serviceaccount:70fa-jenkins:jenkins -n 70fa-parks-prod
+oc new-project ${GUID}-parks-dev  --display-name="${GUID} AdvDev Homework Parks Development"
+oc policy add-role-to-user admin ${USER} -n ${GUID}-parks-dev
 
-# To be Implemented by Student
+oc new-project ${GUID}-parks-prod --display-name="${GUID} AdvDev Homework Parks Production"
+oc policy add-role-to-user admin ${USER} -n ${GUID}-parks-prod
 
-# Replicated MongoDB setup
-echo 'kind: Service
-apiVersion: v1
-metadata:
-  name: "mongodb-internal"
-  labels:
-    name: "mongodb"
-  annotations:
-    service.alpha.kubernetes.io/tolerate-unready-endpoints: "true"
-spec:
-  clusterIP: None
-  ports:
-    - name: mongodb
-      port: 27017
-  selector:
-    name: "mongodb"' | oc create -f -
+oc policy add-role-to-user admin ${USER} -n ${GUID}-nexus
+oc policy add-role-to-user admin ${USER} -n ${GUID}-sonarqube
+oc policy add-role-to-user admin ${USER} -n ${GUID}-jenkins
+oc policy add-role-to-user admin ${USER} -n ${GUID}-parks-dev
+oc policy add-role-to-user admin ${USER} -n ${GUID}-parks-prod
 
+oc annotate namespace ${GUID}-nexus      openshift.io/requester=${USER} --overwrite
+oc annotate namespace ${GUID}-sonarqube  openshift.io/requester=${USER} --overwrite
+oc annotate namespace ${GUID}-jenkins    openshift.io/requester=${USER} --overwrite
+oc annotate namespace ${GUID}-parks-dev  openshift.io/requester=${USER} --overwrite
+oc annotate namespace ${GUID}-parks-prod openshift.io/requester=${USER} --overwrite
 
-
-echo 'kind: Service
-apiVersion: v1
-metadata:
-  name: "mongodb"
-  labels:
-    name: "mongodb"
-spec:
-  ports:
-    - name: mongodb
-      port: 27017
-  selector:
-    name: "mongodb"' | oc create -f -
-
-
-
-echo 'kind: StatefulSet
-apiVersion: apps/v1
-metadata:
-  name: "mongodb"
-spec:
-  serviceName: "mongodb-internal"
-  replicas: 3
-  selector:
-    matchLabels:
-      name: mongodb
-  template:
-    metadata:
-      labels:
-        name: "mongodb"
-    spec:
-      containers:
-        - name: mongo-container
-          image: "registry.access.redhat.com/rhscl/mongodb-34-rhel7:latest"
-          ports:
-            - containerPort: 27017
-          args:
-            - "run-mongod-replication"
-          volumeMounts:
-            - name: mongo-data
-              mountPath: "/var/lib/mongodb/data"
-          env:
-            - name: MONGODB_DATABASE
-              value: "parks"
-            - name: MONGODB_USER
-              value: "mongodb"
-            - name: MONGODB_PASSWORD
-              value: "mongodb"
-            - name: MONGODB_ADMIN_PASSWORD
-              value: "mongodb_admin_password"
-            - name: MONGODB_REPLICA_NAME
-              value: "rs0"
-            - name: MONGODB_KEYFILE_VALUE
-              value: "12345678901234567890"
-            - name: MONGODB_SERVICE_NAME
-              value: "mongodb-internal"
-          readinessProbe:
-            exec:
-              command:
-                - stat
-                - /tmp/initialized
-  volumeClaimTemplates:
-    - metadata:
-        name: mongo-data
-        labels:
-          name: "mongodb"
-      spec:
-        accessModes: [ ReadWriteOnce ]
-        resources:
-          requests:
-            storage: "4Gi"' | oc create -f -
-
-
-# Blue Application
-oc new-app 70fa-parks-dev/mlbparks:0.0 --name=mlbparks-blue -e APPNAME="MLB Parks (Blue)" --allow-missing-imagestream-tags=true
-oc new-app 70fa-parks-dev/nationalparks:0.0 --name=nationalparks-blue -e APPNAME="National Parks (Blue)" --allow-missing-imagestream-tags=true
-oc new-app 70fa-parks-dev/parksmap:0.0 --name=parksmap-blue -e APPNAME="ParksMap (Blue)" --allow-missing-imagestream-tags=true
-
-
-oc set triggers dc/mlbparks-blue --remove-all
-oc set triggers dc/nationalparks-blue --remove-all
-oc set triggers dc/parksmap-blue --remove-all
-
-
-oc expose dc/mlbparks-blue --port 8080
-oc expose dc/nationalparks-blue --port 8080
-oc expose dc/parksmap-blue --port 8080
-
-oc create configmap mlbparks-blue-config --from-literal="application-db.properties=Placeholder"
-oc create configmap nationalparks-blue-config --from-literal="application-db.properties=Placeholder"
-oc create configmap parksmap-blue-config --from-literal="application-db.properties=Placeholder"
-
-# oc env dc/mlbparks-blue --from=configmap/mlbparks-blue-config
-# oc env dc/nationalparks-blue --from=configmap/nationalparks-blue-config
-# oc env dc/parksmap-blue --from=configmap/parksmap-blue-config
-
-oc expose svc/mlbparks-blue --name mlbparks -n 70fa-parks-prod
-oc expose svc/nationalparks-blue --name nationalparks -n 70fa-parks-prod
-oc expose svc/parksmap-blue --name parksmap -n 70fa-parks-prod
-
-
-# Green Application
-oc new-app 70fa-parks-dev/mlbparks:0.0 --name=mlbparks-green -e APPNAME="MLB Parks (Green)" --allow-missing-imagestream-tags=true
-oc new-app 70fa-parks-dev/nationalparks:0.0 --name=nationalparks-green -e APPNAME="National Parks (Green)" --allow-missing-imagestream-tags=true
-oc new-app 70fa-parks-dev/parksmap:0.0 --name=parksmap-green -e APPNAME="ParksMap (Green)" --allow-missing-imagestream-tags=true
-
-
-oc set triggers dc/mlbparks-green --remove-all
-oc set triggers dc/nationalparks-green --remove-all
-oc set triggers dc/parksmap-green --remove-all
-
-
-oc expose dc/mlbparks-green --port 8080
-oc expose dc/nationalparks-green --port 8080
-oc expose dc/parksmap-green --port 8080
-
-oc create configmap mlbparks-green-config --from-literal="application-db.properties=Placeholder"
-oc create configmap nationalparks-green-config --from-literal="application-db.properties=Placeholder"
-oc create configmap parksmap-green-config --from-literal="application-db.properties=Placeholder"
-
-# oc env dc/mlbparks-green --from=configmap/mlbparks-green-config
-# oc env dc/nationalparks-green --from=configmap/nationalparks-green-config
-# oc env dc/parksmap-green --from=configmap/parksmap-green-config
+echo "***************************************************************"
+echo "Completed creation of projects"
+echo "***************************************************************"
