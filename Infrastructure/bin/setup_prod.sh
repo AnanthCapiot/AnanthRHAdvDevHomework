3c3745512413ce1ca0208dfc85e79116f24f10f3
AnanthCapiot <41581468+AnanthCapiot@users.noreply.github.com>
2018-09-27 20:17:00 +0530

    Update setup_prod.sh

--- Infrastructure/bin/setup_prod.sh
+++ Infrastructure/bin/setup_prod.sh
@@ -15,6 +15,7 @@ oc project ${GUID}-parks-prod
 
 oc policy add-role-to-group system:image-puller system:serviceaccounts:${GUID}-parks-prod -n ${GUID}-parks-dev
 oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-parks-prod
+oc policy add-role-to-user edit system:serviceaccount:gpte-jenkins:jenkins -n ${GUID}-parks-prod
 
 # To be Implemented by Student
 
